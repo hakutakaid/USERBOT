a55b34b779934b1508f294d0ab8653f907141dd3
hakutakaid <rachmadalfio@gmail.com>
2023-07-17 20:37:18 +0700

    BOT

---
+++ haku/modules/apmguarde.py
@@ -0,0 +1,35 @@
+from pyrogram import filters, Client
+import asyncio
+#from Zaid import SUDO_USER
+#from Zaid.modules.help import *
+from pyrogram.methods import messages
+from .pmguard import get_arg, denied_users
+
+import haku.database.pmpermitdb as Zaid
+
+
+
+@Client.on_message(filters.command("pmguard", ["."]) & filters.me)
+async def pmguard(client, message):
+    arg = get_arg(message)
+    if not arg:
+        await message.edit("**I only understand on or off**")
+        return
+    if arg == "off":
+        await Zaid.set_pm(False)
+        await message.edit("**PM Guard Deactivated**")
+    if arg == "on":
+        await Zaid.set_pm(True)
+        await message.edit("**PM Guard Activated**")
+@Client.on_message(filters.command("setpmmsg", ["."]) & filters.me)
+async def setpmmsg(client, message):
+    arg = get_arg(message)
+    if not arg:
+        await message.edit("**What message to set**")
+        return
+    if arg == "default":
+        await Zaid.set_permit_message(Zaid.PMPERMIT_MESSAGE)
+        await message.edit("**Anti_PM message set to default**.")
+        return
+    await Zaid.set_permit_message(f"`{arg}`")
+    await message.edit("**Custom anti-pm message set**")
